cc99a5a94f2f1212db8f0ce16cbf718b40294f9c
MikouZonata <kevin.vanas@student.hku.nl>
2020-02-01 12:56:35 +0100

    XInputEX import & Interactable interaces

--- Assets/Scripts/InteractableObjects/BreakableWall.cs
+++ Assets/Scripts/InteractableObjects/BreakableWall.cs
@@ -2,21 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BreakableWall : InteractableObject
+public class BreakableWall : IInteractable, IBreakable
 {
 	public GameObject fullWall;
 	public GameObject brokenWall;
 
 	bool _isBroken = false;
 
-	public override void Interact ()
+	public void Interact ()
 	{
 		if (_isBroken) {
 
 		}
 	}
 
-	public void BreakWall ()
+	public void Break ()
 	{
 
 	}
--- Assets/Scripts/InteractableObjects/IBreakable.cs
+++ Assets/Scripts/InteractableObjects/IBreakable.cs
@@ -1,18 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class IBreakable : MonoBehaviour
+public interface IBreakable
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	void Break ();
 }
