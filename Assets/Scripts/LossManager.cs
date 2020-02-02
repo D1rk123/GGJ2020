947f5dda921fca852eed8298c862039dbdfe75b9
MikouZonata <kevin.vanas@student.hku.nl>
2020-02-02 15:16:39 +0100

    Loss manager

--- Assets/Scripts/LossManager.cs
+++ Assets/Scripts/LossManager.cs
@@ -7,6 +7,9 @@ public class LossManager : MonoBehaviour
 {
 	public GameObject[] breakableWallObjects;
 	public GameObject[] playerObjects;
+	public ParticleSystem[] explosionParticles;
+	public float explodingDuration = 4;
+	public float secondsPerExplosion = .2f;
 
 	IBreakable[] _breakableWalls;
 	IBreakable[] _playerCharacters;
@@ -30,6 +33,28 @@ public class LossManager : MonoBehaviour
 				aPlayerLives = true;
 			}
 		}
+		bool aWallIsFixed = false;
+		foreach (IBreakable wall in _breakableWalls) {
+			if (!wall.GetIsBroken()) {
+				aWallIsFixed = true;
+			}
+		}
+
+		if (!aPlayerLives || !aWallIsFixed) {
+			StartCoroutine(DestroyMountain());
+		}
+	}
+
+	IEnumerator DestroyMountain ()
+	{
+		int numberOfExplosions = Mathf.RoundToInt(explodingDuration / secondsPerExplosion);
+
+		for (int i = 0; i < numberOfExplosions; i++) {
+			explosionParticles[UnityEngine.Random.Range(0, explosionParticles.Length)].Play();
+			yield return new WaitForSeconds(secondsPerExplosion);
+		}
+
+		RestartGame();
 	}
 
 	void RestartGame ()
